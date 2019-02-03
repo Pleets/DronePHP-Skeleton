4fd27f318fb17b0ba328e4678b054ffa125d933a
Darío Rivera <fermius.us@gmail.com>
2019-02-03 13:57:59 -0500

    Change approach to work without /public directory in routes

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-<?php header("Location: public") ?>
+<?php
+
+chdir(dirname('.'));
+
+// Set localtime zone
+date_default_timezone_set("America/Bogota");
+
+// Memory limit
+ini_set("memory_limit","1024M");
+
+// Run application
+require_once("vendor/autoload.php");
+
+function ifdef($value, array $needle)
+{
+    return \Drone\Util\ArrayDimension::ifdef($needle, include 'config/global.config.php', $value);
+}
+
+$config = include "config/application.config.php";
+$mvc = new Drone\Mvc\Application($config);
+$mvc->run();
