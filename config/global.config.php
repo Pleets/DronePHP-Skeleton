fd4c0abae91720a7286db4628839c5d6e1bb776c
Darío Rivera <fermius.us@gmail.com>
2019-02-04 22:28:47 -0500

    Add a global config file

---
+++ config/global.config.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'project' => [
+        'id'   => 'fg5gFPO97HBvcQm5tCn',              # Unique ID for the project
+        'name' => 'My DronePHP project'               # The name of your project
+    ]
+];
